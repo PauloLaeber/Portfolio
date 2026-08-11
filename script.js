104ea168a98a08cd973025e387faa7f6da49642e
PauloLaeber <paulo.batistalaeber@gmail.com>
2026-08-11 15:56:09 -0300

    Modificação do comportamento do submenu, adição de animação ao submenu

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoProjetos = document.querySelector(".botao-projetos");
+const menuProjetos = document.querySelector(".menu-projetos");
+
+botaoProjetos.addEventListener("click", function () {
+  menuProjetos.classList.toggle("aberto");
+})
