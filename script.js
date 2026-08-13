d23d363cbe8e9081aeb48275673e10438faaa945
PauloLaeber <paulo.batistalaeber@gmail.com>
2026-08-13 16:45:15 -0300

    Destacamento de secoes ativas

--- script.js
+++ script.js
@@ -5,4 +5,35 @@ botoesMenu.forEach(function (botao) {
     const menu = botao.parentElement;
     menu.classList.toggle('aberto');
   })
-})
+});
+
+
+const linksSecao = document.querySelectorAll('.menu-direito .link-menu[href^="#"]');
+const secoes = document.querySelectorAll('.conteudo section[id]');
+
+function atualizarSecaoAtiva() {
+  let secaoAtiva = null;
+
+  secoes.forEach(function (secao) {
+    const posicao = secao.getBoundingClientRect();
+
+    if (posicao.top <= window.innerHeight / 2 && posicao.bottom >= window.innerHeight / 2) {
+      secaoAtiva = secao;
+    }
+  });
+
+  linksSecao.forEach(function (link) {
+    link.classList.remove("ativo");
+  });
+
+  if (secaoAtiva) {
+    const linkAtivo = document.querySelector('.menu-direito .link-menu[href="#${secaoAtiva.id}"]');
+
+    if (linkAtivo) {
+      linkAtivo.classList.add("ativo");
+    }
+  }
+};
+
+window.addEventListener("scroll", atualizarSecaoAtiva);
+atualizarSecaoAtiva();
